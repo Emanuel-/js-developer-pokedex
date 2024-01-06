6183c6e199655135971202b18d17a666ea52364c
Emanuel Albuqueruque <emanuel.albuquerque.1995@gmail.com>
2024-01-06 01:12:50 -0300

    Projeto com as modifcações solicitadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,8 @@ function convertPokemonToLi(pokemon) {
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
-            <div class="detail">
+            <div class="detail" style="user-select: none;">
+                
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
@@ -19,6 +20,31 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <div style="display: none;">
+                <div id="hab1">${pokemon.abilities[0]["name"]}</div>
+                <div id="deschab1">${pokemon.abilities[0]["effect"]}</div>
+                <div id="hab2">${pokemon.abilities[1]["name"]}</div>
+                <div id="deschab2">${pokemon.abilities[1]["effect"]}</div>
+
+                <div id="stat1">${pokemon.stats[0]["name"]}</div>
+                <div id="descstat1">${pokemon.stats[0]["baseValue"]}</div>
+                <div id="stat2">${pokemon.stats[1]["name"]}</div>
+                <div id="descstat2">${pokemon.stats[1]["baseValue"]}</div>
+                <div id="stat3">${pokemon.stats[2]["name"]}</div>
+                <div id="descstat3">${pokemon.stats[2]["baseValue"]}</div>
+                <div id="stat4">${pokemon.stats[3]["name"]}</div>
+                <div id="descstat4">${pokemon.stats[3]["baseValue"]}</div>
+                <div id="stat5">${pokemon.stats[4]["name"]}</div>
+                <div id="descstat5">${pokemon.stats[4]["baseValue"]}</div>
+                <div id="stat6">${pokemon.stats[5]["name"]}</div>
+                <div id="descstat6">${pokemon.stats[5]["baseValue"]}</div>
+
+                
+              
+
+
+            </div>
+
         </li>
     `
 }
@@ -32,6 +58,81 @@ function loadPokemonItens(offset, limit) {
 
 loadPokemonItens(offset, limit)
 
+
+
+$('#pokemonList').on('click', 'li', function() {
+    const pokemonId = $(this).data('id');
+    const pokemonName = $(this).find('.name').text();
+    const pokemonNumber = $(this).find('.number').text();
+    const pokemonClass =$(this).find('.types li').map(function() {
+        return $(this).text();
+    }).get().join(' ');
+    const pokemonPhoto = $(this).find('img').attr('src');
+    console.log('Aqui imagem', pokemonPhoto);
+    const pokemonColor = getPokemonColor(pokemonClass.split(' ')[0]); // Função para obter a cor do cartão
+    const pokemonAbility1 = $(this).find('#hab1').text();
+    const pokemonDescAbility1 = $(this).find('#deschab1').text();
+    const pokemonAbility2 = $(this).find('#hab2').text();
+    const pokemonDescAbility2 = $(this).find('#deschab2').text();
+    const pokemonHP = $(this).find('#descstat1').text();
+    const pokemonAttack = $(this).find('#descstat2').text();
+    const pokemonDeffense = $(this).find('#descstat3').text();
+    const pokemonSpecialAttack = $(this).find('#descstat4').text();
+    const pokemonSpecialDefense = $(this).find('#descstat5').text();
+    const pokemonSpeed = $(this).find('#descstat6').text();
+
+
+
+    // Exibir informações no console (você pode personalizar isso conforme necessário)
+    console.log('ID do Pokémon:', pokemonId);
+    console.log('Nome do Pokémon:', pokemonName);
+    console.log('Classe do Pokémon:', pokemonClass);
+    console.log('Número do Pokémon:', pokemonNumber);
+
+    // Exibir informações no modal
+    $('#pokemonId').text(pokemonNumber);
+    $('#pokemonName').text(capitalizeFirstLetter(pokemonName));
+    $('#pokemonNumber').text(pokemonNumber);
+    $('#pokemonClass').text(pokemonClass);
+    $('#move1 h3').text(capitalizeFirstLetter(pokemonAbility1));
+    $('#move1 p').text(capitalizeFirstLetter(pokemonDescAbility1));
+    $('#move2 h3').text(capitalizeFirstLetter(pokemonAbility2));
+    $('#move2 p').text(capitalizeFirstLetter(pokemonDescAbility2));
+    $('[aria-label="HP example"] div').text(pokemonHP)
+    $('[aria-label="Attack example"] div').text(pokemonAttack)
+    $('[aria-label="Defense example"] div').text(pokemonDeffense)
+    $('[aria-label="Special Attack example"] div').text(pokemonSpecialAttack)
+    $('[aria-label="Special Defense example"] div').text(pokemonSpecialDefense)
+    $('[aria-label="Speed example"] div').text(pokemonSpeed)
+
+
+
+    // Atualizar a foto do Pokémon e a cor do cartão
+    $('#pokemonImage').attr('src', pokemonPhoto); // Atualiza o src da imagem
+    $('#pokemonImage').attr('alt',  pokemonName); // Atualiza o alt da imagem
+    $('[aria-label="HP example"] div').attr('style', 'width:'+preencheBarra(pokemonHP)+'%');
+    $('[aria-label="Attack example"] div').attr('style', 'width:'+preencheBarra(pokemonAttack)+'%');
+    $('[aria-label="Defense example"] div').attr('style', 'width:'+preencheBarra(pokemonDeffense)+'%');
+    $('[aria-label="Special Attack example"] div').attr('style', 'width:'+preencheBarra(pokemonSpecialAttack)+'%');
+    $('[aria-label="Special Defense example"] div').attr('style', 'width:'+preencheBarra(pokemonSpecialDefense)+'%');
+    $('[aria-label="Speed example"] div').attr('style', 'width:'+preencheBarra(pokemonSpeed)+'%');
+    $('.modal-header').css('background-color', pokemonColor);
+    $('.modal-header').css('color', 'white');
+
+
+
+    //$('#pokemonImage').css('background-image', `url(${pokemonPhoto})`);
+    //$('#cardColor').css('background-color', pokemonColor);
+
+    // Definir o título do modal como o nome do Pokémon clicado
+    $('#TituloModalCentralizado').text(`Detalhes do Pokémon - ${capitalizeFirstLetter(pokemonName)}`);
+
+    // Abrir o modal
+    var myModal = new bootstrap.Modal(document.getElementById('ExemploModalCentralizado'));
+    myModal.show();
+  });
+
+  
 loadMoreButton.addEventListener('click', () => {
     offset += limit
     const qtdRecordsWithNexPage = offset + limit
@@ -45,3 +146,52 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+function getPokemonColor(pokemonType) {
+    // Mapeia os tipos de Pokémon para as cores correspondentes
+    const typeToColorMap = {
+        'grass': '#77c850',
+        'fire': '#ee7f30',
+        'water': '#678fee',
+        'electric': '#f7cf2e',
+        'ice': '#98d5d7',
+        'ground': '#dfbf69',
+        'flying': '#a98ff0',
+        'poison': '#a040a0',
+        'fighting': '#bf3029',
+        'psychic': '#f65687',
+        'dark': '#725847',
+        'rock': '#b8a137',
+        'bug': '#a8b720',
+        'ghost': '#6e5896',
+        'steel': '#b9b7cf',
+        'dragon': '#6f38f6',
+        'fairy': '#f9aec7'
+    };
+
+    // Verifica se o tipo do Pokémon está mapeado
+    if (typeToColorMap.hasOwnProperty(pokemonType)) {
+        return typeToColorMap[pokemonType];
+    }
+
+    // Retorna uma cor padrão se o tipo não estiver mapeado
+    return '#a6a877'; // ou qualquer cor padrão desejada
+}
+
+function preencheBarra(hab){
+    return (parseFloat(hab)/2).toString();
+}
+
+
+
+//function abrirModal() {
+//    // Abrir o modal quando o item da lista for clicado
+//    var myModal = new bootstrap.Modal(document.getElementById('ExemploModalCentralizado'));
+//    myModal.show();
+//  }
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,4 +1,3 @@
-
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
@@ -6,15 +5,53 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
 
+    
+
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
-    return pokemon
+    // Mapeia as habilidades e guarda no array abilities do Pokemon
+    const abilitiesPromises = pokeDetail.abilities.map((ability) => {
+        const abilityUrl = ability.ability.url;
+        return fetch(abilityUrl)
+            .then(response => response.json())
+            .then(abilityDetails => ({
+                name: ability.ability.name,
+                url: abilityUrl,
+                effect: abilityDetails.effect_entries[0].effect,
+                // Adicione outras propriedades que você deseja extrair da URL da habilidade
+                // Exemplo: hidden: abilityDetails.is_hidden
+            }));
+    });
+
+
+    // Mapeia as estatísticas e guarda no array stats do Pokemon
+    const stats = pokeDetail.stats.map(stat => ({
+        name: stat.stat.name,
+        baseValue: stat.base_stat,
+    }));
+
+    pokemon.stats = stats;
+
+    // Adiciona a propriedade evolution_chain.url à instância do Pokemon
+    pokemon.evolutionChainUrl = pokeDetail.species.url;
+
+    // Aguarda todas as chamadas assíncronas das URLs das habilidades
+    return Promise.all(abilitiesPromises)
+        .then(abilitiesDetails => {
+            pokemon.abilities = abilitiesDetails;
+            console.log('Abilidades aqui', pokemon.abilities);
+            console.log('Stats aqui', pokemon.stats);
+            console.log('Evolução aqui', pokemon.evolutionChainUrl);
+
+            console.log(pokeDetail);
+            return pokemon;
+        });
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
@@ -32,4 +69,11 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)
+}
+
+
+// Função para obter informações de evolução
+pokeApi.getEvolutionChain = (evolutionChainUrl) => {
+    return fetch(evolutionChainUrl)
+        .then(response => response.json());
 }
